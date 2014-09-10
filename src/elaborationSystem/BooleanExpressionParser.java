6f74bf3e6dcbe254b94808e9717d7fabc0566014
jacoposq <jacoposquillaci@gmail.com>
2014-09-10 20:05:40 +0200

    For Each risolto in modo molto provvisorio

--- src/elaborationSystem/BooleanExpressionParser.java
+++ src/elaborationSystem/BooleanExpressionParser.java
@@ -4,13 +4,14 @@ public class BooleanExpressionParser {
 
 	public static void main(String[] args) {
 		
-		String[] a = new String[6];
+		String[] a = new String[7];
 		a[0] = "if(x == 3 && tested ){";
 		a[1] = "else if( (y -3) != 4 || y == 2 && alfa != 9)";
 		a[2] = "for(int i = 0; i< 10; i++){";
 		a[3] = "if(stack.isempty() | list.size() == 0)";
 		a[4] = "if(stack.isempty())";
 		a[5] = "for(int i = 0; i< 10 & list.size() == 0; i++){";
+		a[6] = "for(String str:lista) {";
 		
 		for (int i = 0; i < a.length; i++) {
 			System.out.println(extractOperands(a[i]));
@@ -49,13 +50,17 @@ public class BooleanExpressionParser {
 				break;
 			}
 		}
-		//se non e' condizione di un for
-		if(!string.substring(indexOpen+1,indexClosed).contains(";")){
-		return string.substring(indexOpen+1,indexClosed);
-		}else{
-			//condizione for
+		//condizione for
+		
+		if(string.substring(indexOpen+1,indexClosed).contains(";")){
 			String[] innerFor=string.substring(indexOpen+1,indexClosed).split(";");
 			return innerFor[1];
+		
+		}else if(string.substring(indexOpen+1,indexClosed).contains(":")){
+			//se non e' condizione di un for
+			return "forEach";
+		}else{
+			return string.substring(indexOpen+1,indexClosed);
 		}
 	}
 
